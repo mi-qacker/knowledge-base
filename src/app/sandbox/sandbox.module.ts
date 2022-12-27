363ac9b62df821634241846250653084b09f1e18
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-12-27 12:14:16 +0300

    create sandbox posts list

--- src/app/sandbox/sandbox.module.ts
+++ src/app/sandbox/sandbox.module.ts
@@ -1,5 +1,8 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import {NewPostComponent} from './pages/new-post/new-post.component';
 import {PostsListComponent} from './pages/posts-list/posts-list.component';
@@ -7,6 +10,12 @@ import {SandboxRoutingModule} from './sandbox-routing.module';
 
 @NgModule({
   declarations: [NewPostComponent, PostsListComponent],
-  imports: [CommonModule, SandboxRoutingModule],
+  imports: [
+    CommonModule,
+    SandboxRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
 })
 export class SandboxModule {}
