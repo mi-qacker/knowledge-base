3b30fdf23399cecd2acc6425e5b8a6e27fa53019
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-12-27 12:52:13 +0300

    add basic editor

---
+++ src/app/sandbox/pages/new-post/editor.config.ts
@@ -0,0 +1,18 @@
+// @ts-ignore
+import Embed from '@editorjs/embed';
+// @ts-ignore
+import Header from '@editorjs/header';
+// @ts-ignore
+import List from '@editorjs/list';
+// @ts-ignore
+import Marker from '@editorjs/marker';
+
+export const editorjsConfig = {
+  holder: 'editorjs',
+  tools: {
+    Marker: Marker,
+    header: Header,
+    list: List,
+    embed: Embed,
+  },
+};
--- src/app/sandbox/pages/new-post/new-post.component.ts
+++ src/app/sandbox/pages/new-post/new-post.component.ts
@@ -1,10 +1,51 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import EditorJS from '@editorjs/editorjs';
+import {Observable} from 'rxjs';
+import {debounceTime, skip} from 'rxjs/operators';
+
+import {editorjsConfig} from './editor.config';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.scss'],
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit, OnDestroy {
+  editorData: any;
+  editor!: EditorJS;
+  editorObserver!: MutationObserver;
+
   constructor() {}
+  ngOnInit(): void {
+    this.editor = new EditorJS(editorjsConfig);
+
+    this.detectEditorChanges()
+      .pipe(debounceTime(200), skip(1))
+      .subscribe(data => {
+        this.editor.save().then(outputData => {
+          this.editorData = JSON.stringify(outputData, null, 2);
+        });
+      });
+  }
+  ngOnDestroy(): void {
+    this.editorObserver.disconnect();
+  }
+
+  saveEditorData(): void {
+    this.editor.save().then(outputData => {
+      this.editorData = JSON.stringify(outputData, null, 2);
+    });
+  }
+
+  detectEditorChanges(): Observable<any> {
+    return new Observable(observer => {
+      const editorDom = document.querySelector('#editorjs');
+      const config = {attributes: true, childList: true, subtree: true};
+
+      this.editorObserver = new MutationObserver(mutation => {
+        observer.next(mutation);
+      });
+      if (editorDom) this.editorObserver.observe(editorDom, config);
+    });
+  }
 }
