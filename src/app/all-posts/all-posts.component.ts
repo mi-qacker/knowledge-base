a7ed7c35775b1623c84366275fcea5e9fef093ca
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-12-29 00:14:01 +0300

    add all posts

--- src/app/all-posts/all-posts.component.ts
+++ src/app/all-posts/all-posts.component.ts
@@ -1,17 +1,24 @@
 import {CommonModule} from '@angular/common';
 import {Component, OnInit} from '@angular/core';
+import {map} from 'rxjs/operators';
 
+import {LoggedUserService} from '../auth/services/logged-user/logged-user.service';
 import {HttpModule} from '../http/http.module';
 import {PostHttpService} from '../http/services/post-http/post-http.service';
+import {PostModule} from '../post/post.module';
 
 @Component({
   selector: 'app-all-posts',
   standalone: true,
-  imports: [CommonModule, HttpModule],
+  imports: [CommonModule, HttpModule, PostModule],
   templateUrl: './all-posts.component.html',
   styleUrls: ['./all-posts.component.scss'],
 })
 export class AllPostsComponent {
   posts$ = this.postHttpService.getPosts(true);
-  constructor(private postHttpService: PostHttpService) {}
+  userId$ = this.loggedUserService.user$.pipe(map(user => user?._id));
+  constructor(
+    private postHttpService: PostHttpService,
+    private loggedUserService: LoggedUserService
+  ) {}
 }
