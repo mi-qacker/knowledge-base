5044970aea8f04d351ec31e63bbdc2b886b34f8b
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-12-29 10:32:29 +0300

    add list animations

--- src/app/all-posts/all-posts.component.ts
+++ src/app/all-posts/all-posts.component.ts
@@ -1,9 +1,18 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import {CommonModule} from '@angular/common';
 import {Component, OnInit} from '@angular/core';
 import {map} from 'rxjs/operators';
 
 import {LoggedUserService} from '../auth/services/logged-user/logged-user.service';
 import {HttpModule} from '../http/http.module';
+import {IPost} from '../http/services/post-http/post.interface';
 import {PostHttpService} from '../http/services/post-http/post-http.service';
 import {PostModule} from '../post/post.module';
 
@@ -13,12 +22,33 @@ import {PostModule} from '../post/post.module';
   imports: [CommonModule, HttpModule, PostModule],
   templateUrl: './all-posts.component.html',
   styleUrls: ['./all-posts.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({opacity: 0}),
+            stagger(100, [animate('0.25s', style({opacity: 1}))]),
+          ],
+          {optional: true}
+        ),
+      ]),
+    ]),
+  ],
 })
-export class AllPostsComponent {
-  posts$ = this.postHttpService.getPosts(true);
+export class AllPostsComponent implements OnInit {
+  posts: IPost[] = [];
   userId$ = this.loggedUserService.user$.pipe(map(user => user?._id));
+
   constructor(
     private postHttpService: PostHttpService,
     private loggedUserService: LoggedUserService
   ) {}
+
+  ngOnInit(): void {
+    this.postHttpService.getPosts(true).subscribe(posts => {
+      posts.forEach(post => this.posts.push(post));
+    });
+  }
 }
--- src/app/sandbox/pages/posts-list/posts-list.component.ts
+++ src/app/sandbox/pages/posts-list/posts-list.component.ts
@@ -1,5 +1,14 @@
-import {Component} from '@angular/core';
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {Component, OnInit} from '@angular/core';
 import {map} from 'rxjs';
+import {IPost} from 'src/app/http/services/post-http/post.interface';
 
 import {LoggedUserService} from '../../../auth/services/logged-user/logged-user.service';
 import {PostHttpService} from '../../../http/services/post-http/post-http.service';
@@ -8,12 +17,32 @@ import {PostHttpService} from '../../../http/services/post-http/post-http.servic
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({opacity: 0}),
+            stagger(100, [animate('0.25s', style({opacity: 1}))]),
+          ],
+          {optional: true}
+        ),
+      ]),
+    ]),
+  ],
 })
-export class PostsListComponent {
-  posts$ = this.postHttpService.getPosts(false);
+export class PostsListComponent implements OnInit {
+  posts: IPost[] = [];
   userId$ = this.loggedUserService.user$.pipe(map(user => user?._id));
   constructor(
     private postHttpService: PostHttpService,
     private loggedUserService: LoggedUserService
   ) {}
+
+  ngOnInit(): void {
+    this.postHttpService
+      .getPosts(false)
+      .subscribe(posts => posts.forEach(post => this.posts.push(post)));
+  }
 }
