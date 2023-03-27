05f39e5c16837eedc30b0b81ae813e1da9a173b5
Ксения Ильницкая <ilnitskaya2001@gmail.com>
2023-03-27 22:53:53 +0300

    профиль модератора

--- src/app/pages/profile-moderator-page/profile-moderator-page.component.ts
+++ src/app/pages/profile-moderator-page/profile-moderator-page.component.ts
@@ -1,8 +1,34 @@
-import {Component} from '@angular/core';
+/* eslint-disable prettier/prettier */
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { shareReplay } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import { AuthHttpService } from '../../services/http/auth-http/auth-http.service';
+import { ICategory } from '../../services/http/category-http/category';
+import { CategoryHttpService } from '../../services/http/category-http/category-http.service';
 
 @Component({
   selector: 'app-profile-moderator-page',
   templateUrl: './profile-moderator-page.component.html',
   styleUrls: ['./profile-moderator-page.component.scss'],
 })
-export class ProfileModeratorPageComponent {}
+export class ProfileModeratorPageComponent implements OnInit {
+  user$ = this.authHttpService.getLoggedUser().pipe(shareReplay(1));
+  categories$!: Observable<ICategory[]>;
+  userID!: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private authHttpService: AuthHttpService,
+    private categoryHttpService: CategoryHttpService
+  ) {
+    this.route.params.subscribe(({ id }) => {
+      this.userID = id;
+    });
+  }
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryHttpService.getCategories();
+  }
+}
--- src/app/pages/profile-moderator-page/profile-moderator-page.module.ts
+++ src/app/pages/profile-moderator-page/profile-moderator-page.module.ts
@@ -1,11 +1,26 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
+/* eslint-disable simple-import-sort/imports */
+/* eslint-disable prettier/prettier */
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
-import {ProfileModeratorPageComponent} from './profile-moderator-page.component';
-import {ProfileModeratorPageRoutingModule} from './profile-moderator-page-routing.module';
+import { ProfileModeratorPageComponent } from './profile-moderator-page.component';
+import { ProfileModeratorPageRoutingModule } from './profile-moderator-page-routing.module';
+import { PostModule } from "../../widgets/post/post.module";
 
 @NgModule({
   declarations: [ProfileModeratorPageComponent],
-  imports: [CommonModule, ProfileModeratorPageRoutingModule],
+  imports: [
+    CommonModule,
+    ProfileModeratorPageRoutingModule,
+    MatDividerModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    PostModule
+  ]
 })
-export class ProfileModeratorPageModule {}
+export class ProfileModeratorPageModule { }
