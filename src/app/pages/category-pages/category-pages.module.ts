d2e898f6d11a489e4789a3e2ec5500db529002e5
Ксения Ильницкая <ilnitskaya2001@gmail.com>
2023-03-28 14:52:42 +0300

    list of categories

--- src/app/pages/category-pages/category-pages.module.ts
+++ src/app/pages/category-pages/category-pages.module.ts
@@ -1,12 +1,21 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
-import {CategoriesListPageComponent} from './categories-list-page/categories-list-page.component';
-import {CategoryPageComponent} from './category-page/category-page.component';
-import {CategoryPagesRoutingModule} from './category-pages-routing.module';
+import { CategoriesListPageComponent } from './categories-list-page/categories-list-page.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { CategoryPagesRoutingModule } from './category-pages-routing.module';
 
 @NgModule({
   declarations: [CategoryPageComponent, CategoriesListPageComponent],
-  imports: [CommonModule, CategoryPagesRoutingModule],
+  imports: [
+    CommonModule,
+    CategoryPagesRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
 })
-export class CategoryPagesModule {}
+export class CategoryPagesModule { }
