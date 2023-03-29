660dbf435fd45af0191c72ce99c0bd29db539654
Ксения Ильницкая <ilnitskaya2001@gmail.com>
2023-03-29 20:23:09 +0300

    list of users

--- src/app/pages/admin-pages/admin-users-page/admin-users-page.component.ts
+++ src/app/pages/admin-pages/admin-users-page/admin-users-page.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-users-page',
   templateUrl: './admin-users-page.component.html',
   styleUrls: ['./admin-users-page.component.scss'],
 })
-export class AdminUsersPageComponent {}
+export class AdminUsersPageComponent { }
