58d42fec01af684f2b5132ab20dd0018a9ddfd46
Ксения Ильницкая <ilnitskaya2001@gmail.com>
2023-03-29 20:22:53 +0300

    list of category pages

--- src/app/pages/admin-pages/admin-pages.module.ts
+++ src/app/pages/admin-pages/admin-pages.module.ts
@@ -1,12 +1,21 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
-import {AdminCategoriesPageComponent} from './admin-categories-page/admin-categories-page.component';
-import {AdminPagesRoutingModule} from './admin-pages-routing.module';
-import {AdminUsersPageComponent} from './admin-users-page/admin-users-page.component';
+import { AdminCategoriesPageComponent } from './admin-categories-page/admin-categories-page.component';
+import { AdminPagesRoutingModule } from './admin-pages-routing.module';
+import { AdminUsersPageComponent } from './admin-users-page/admin-users-page.component';
 
 @NgModule({
   declarations: [AdminUsersPageComponent, AdminCategoriesPageComponent],
-  imports: [CommonModule, AdminPagesRoutingModule],
+  imports: [
+    CommonModule,
+    AdminPagesRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
-export class AdminPagesModule {}
+export class AdminPagesModule { }
