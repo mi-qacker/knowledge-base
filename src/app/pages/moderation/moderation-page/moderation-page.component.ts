aeb3a3b32fd3777a5f8e63aa6a6b42d98c01097f
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2023-04-07 17:06:13 +0300

    отображение постов для модерации предварительно

--- src/app/pages/moderation/moderation-page/moderation-page.component.ts
+++ src/app/pages/moderation/moderation-page/moderation-page.component.ts
@@ -1,8 +1,49 @@
 import {Component} from '@angular/core';
+import {LoggedUserService} from 'app/services/auth/logged-user-service/logged-user.service';
+import {ICategory} from 'app/services/http/category-http/category';
+import {CategoryHttpService} from 'app/services/http/category-http/category-http.service';
+import {BehaviorSubject, filter, Observable, of, switchMap} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import {PostHttpService} from '../../../services/http/post-http/post-http.service';
 
 @Component({
   selector: 'app-moderation-page',
   templateUrl: './moderation-page.component.html',
   styleUrls: ['./moderation-page.component.scss'],
 })
-export class ModerationPageComponent {}
+export class ModerationPageComponent {
+  userId$ = this.loggedUserService.user$.pipe(map(user => user?._id));
+  categories$: Observable<undefined | ICategory[]>;
+  selectedCategory$ = new BehaviorSubject<string | undefined>(undefined);
+  selectedCategory?: string;
+  posts$ = this.selectedCategory$.pipe(
+    filter(selected => selected !== undefined),
+    switchMap(selected => {
+      this.selectedCategory = selected;
+      return this.postHttpService.getPosts({
+        moderation: false,
+        categoryId: selected,
+      });
+    })
+  );
+
+  constructor(
+    private loggedUserService: LoggedUserService,
+    private categoryHttpService: CategoryHttpService,
+    private postHttpService: PostHttpService
+  ) {
+    this.categories$ = this.loggedUserService.knowledgeUser$.pipe(
+      switchMap(user => {
+        if (!user) return of(undefined);
+        return user.mainAdmin
+          ? this.categoryHttpService.getCategories()
+          : of(user.categoriesAdmin);
+      })
+    );
+  }
+
+  selectCategory(categoryId: string) {
+    this.selectedCategory$.next(categoryId);
+  }
+}
--- src/app/pages/moderation/moderation.module.ts
+++ src/app/pages/moderation/moderation.module.ts
@@ -1,11 +1,13 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatListModule} from '@angular/material/list';
 
+import {PostModule} from '../../widgets/post/post.module';
 import {ModerationPageComponent} from './moderation-page/moderation-page.component';
 import {ModerationRoutingModule} from './moderation-routing.module';
 
 @NgModule({
   declarations: [ModerationPageComponent],
-  imports: [CommonModule, ModerationRoutingModule],
+  imports: [CommonModule, ModerationRoutingModule, MatListModule, PostModule],
 })
 export class ModerationModule {}
