a8b9ba769ae1b6c146302401e27da9cbc1f6dbb0
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2023-05-11 15:58:17 +0300

    titles for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'posts'},
   {
     path: 'posts',
+    title: 'Публикации',
     loadComponent: () =>
       import('./pages/all-posts/all-posts.component').then(
         c => c.AllPostsComponent
@@ -12,6 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'maps',
+    title: 'Дорожные карты',
     children: [
       {
         path: '',
@@ -32,6 +34,7 @@ const routes: Routes = [
   },
   {
     path: 'login',
+    title: 'Войти',
     loadComponent: () =>
       import('./pages/login-page/login-page.component').then(
         c => c.LoginPageComponent
@@ -39,6 +42,7 @@ const routes: Routes = [
   },
   {
     path: 'registration',
+    title: 'Регистрация',
     loadComponent: () =>
       import('./pages/registration-page/registration-page.component').then(
         c => c.RegistrationPageComponent
@@ -46,6 +50,7 @@ const routes: Routes = [
   },
   {
     path: 'profile',
+    title: 'Профиль',
     loadChildren: () =>
       import('./pages/profile-page/profile-page.module').then(
         m => m.ProfilePageModule
@@ -53,6 +58,7 @@ const routes: Routes = [
   },
   {
     path: 'moderator',
+    title: 'Страница модератора',
     loadChildren: () =>
       import(
         './pages/profile-moderator-page/profile-moderator-page.module'
@@ -60,6 +66,7 @@ const routes: Routes = [
   },
   {
     path: 'moderation',
+    title: 'Лаборатория',
     loadChildren: () =>
       import('./pages/moderation/moderation.module').then(
         m => m.ModerationModule
@@ -67,6 +74,7 @@ const routes: Routes = [
   },
   {
     path: 'new',
+    title: 'Создание публикации',
     loadComponent: () =>
       import('./pages/editor-page/editor-page.component').then(
         c => c.EditorPageComponent
@@ -74,11 +82,13 @@ const routes: Routes = [
   },
   {
     path: 'post',
+    title: 'Публикация',
     loadChildren: () =>
       import('./pages/post-page/post-page.module').then(m => m.PostPageModule),
   },
   {
     path: 'category',
+    title: 'Категории',
     loadChildren: () =>
       import('./pages/category-pages/category-pages.module').then(
         m => m.CategoryPagesModule
@@ -86,6 +96,7 @@ const routes: Routes = [
   },
   {
     path: 'admin',
+    title: 'Панель администратора',
     loadChildren: () =>
       import('./pages/admin-pages/admin-pages.module').then(
         m => m.AdminPagesModule
