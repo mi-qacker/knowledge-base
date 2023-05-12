00ca2895c55eb40de5f2c985ff31c4377534c471
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2023-05-12 23:36:58 +0300

    add mock tree

--- src/app/widgets/road-map-tree/road-map-tree.component.ts
+++ src/app/widgets/road-map-tree/road-map-tree.component.ts
@@ -1,11 +1,60 @@
+import {NestedTreeControl} from '@angular/cdk/tree';
 import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTreeModule, MatTreeNestedDataSource} from '@angular/material/tree';
+
+interface Node {
+  title: string;
+  child?: Node[];
+}
 
 @Component({
   selector: 'app-road-map-tree',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTreeModule],
   templateUrl: './road-map-tree.component.html',
   styleUrls: ['./road-map-tree.component.scss'],
 })
-export class RoadMapTreeComponent {}
+export class RoadMapTreeComponent {
+  treeControl = new NestedTreeControl<Node>(node => node.child);
+  dataSource = new MatTreeNestedDataSource<Node>();
+  hasChild = (_: number, node: Node) => !!node.child && node.child.length > 0;
+
+  constructor() {
+    this.dataSource.data = this.treeDataMock;
+  }
+
+  treeDataMock: Node[] = [
+    {
+      title: 'Internet',
+      child: [
+        {title: 'What is HTTP?'},
+        {title: 'Browsers'},
+        {title: 'DNS'},
+        {title: 'Domain Name'},
+        {title: 'Hosting'},
+      ],
+    },
+    {
+      title: 'HTML',
+      child: [
+        {title: 'HTML Basics'},
+        {title: 'Semantic HTML'},
+        {title: 'Forms and Validations'},
+        {title: 'Best Practices'},
+        {title: 'Accessibility'},
+        {title: 'Basics of SEO'},
+      ],
+    },
+    {
+      title: 'CSS',
+      child: [
+        {title: 'CSS Basics'},
+        {title: 'Making layouts', child: [{title: 'Making layouts'}]},
+        {title: 'Responsive Web Design'},
+      ],
+    },
+  ];
+}
